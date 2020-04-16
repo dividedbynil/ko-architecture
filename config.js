69a9d64512968ed8c715a4e79acc0a75950e024f
Kane ONG <dividedbynil@gmail.com>
2020-04-16 17:31:00 +0800

    fix code indentation issue

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	saltRounds: Math.floor(Math.random()*10),
+  saltRounds: Math.floor(Math.random()*10),
   jwtSecretSalt: [...Array(9)].map(() => Math.random().toString(36)[2]).join(''),
   devMongoUrl: 'mongodb://localhost/kane',
   prodMongoUrl: 'mongodb://localhost/kane',
